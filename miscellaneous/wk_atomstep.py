f29785d8a09ff81019987321b634d7287f3284f4
Louislu78 <lewgq@yahoo.com>
2019-12-23 21:24:29 +0800

    These are the modules I build for further usage.

---
+++ miscellaneous/wk_atomstep.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# author: Guangqiang Lu time:2019/12/23
+# If not explicitly pointed out, all the codes are written by myself.
+
+import wikipedia
+
+print(wikipedia.summary('atom bomb'))
+print(wikipedia.search('atom bomb', results=5))
