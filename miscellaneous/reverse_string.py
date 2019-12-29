1f2dc59076d1a6af716b17a06e2bc7700ad6780a
Louislu78 <lewgq@yahoo.com>
2019-12-29 10:12:04 +0800

    These are the modules I build for further usage.

--- miscellaneous/reverse_string.py
+++ miscellaneous/reverse_string.py
@@ -5,14 +5,18 @@
 
 s='accuracy of the information'
 #method one
-s_list=[0]*len(s)
+s_list=['']*len(s)
 print(s_list)
 j=len(s)-1
-for i in range(len(s)):
-    if i<=j:
-        s_list[j]=s[i]
-        s_list[i]=s[j]
-        j-=1
+# for i in range(len(s)):
+#     if i<=j:
+#         s_list[j]=s[i]
+#         s_list[i]=s[j]
+#         j-=1
+# we can also use the following loop as
+for c in s:
+    s_list[j]=c
+    j-=1
 print(s_list)
 s_reversed=''.join(s_list)
 print(s_reversed)
