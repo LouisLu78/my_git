e48edf1de0f31f42d11ea80dbb83c47994dcf5b4
Louislu78 <lewgq@yahoo.com>
2020-01-13 22:52:41 +0800

    These are the modules I build for further usage.

---
+++ miscellaneous/pycb1.2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# author: Guangqiang Lu time:2020/1/13
+# Email: gq4350lu@hotmail.com
+
+
+records = [
+    ('foo', 1, 2),
+    ('bar', 'hello'),
+    ('foo', 3, 4),
+]
+
+def do_foo(x, y):
+    print('foo', x, y)
+
+def do_bar(s):
+    print('bar', s)
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
