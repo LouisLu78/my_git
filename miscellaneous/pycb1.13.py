4bba81acc757bba1ed8a66dce5574380d0997b4b
Louislu78 <lewgq@yahoo.com>
2020-01-26 22:21:27 +0800

    These are the modules I build for further usage.

---
+++ miscellaneous/pycb1.13.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# author: Guangqiang Lu time:2020/1/26
+# Email: gq4350lu@hotmail.com
+
+from operator import itemgetter
+rows = [
+{'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
+{'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
+{'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
+{'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
+]
+
+rows_by_fname = sorted(rows, key=itemgetter('fname'))
+rows_by_uid = sorted(rows, key=itemgetter('uid'))
+print(rows_by_fname)
+print(rows_by_uid)
