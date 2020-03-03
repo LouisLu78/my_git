c33c45ab764cbc96a8f4ff14549115582bcb6022
Louislu78 <lewgq@yahoo.com>
2020-03-03 21:31:43 +0800

    My Python 2020

--- miscellaneous/Q11.13c_V2.py
+++ miscellaneous/Q11.13c_V2.py
@@ -33,7 +33,7 @@ def phoneWords(number):
 
 def main():
 
-    number=2535796
+    number = 53479365569
     phoneWord = list(phoneWords(number))
     chunk=[list(phoneWord[i: i+20]) for i in range (0,len(phoneWord),20)]
     for shortlist in chunk:
